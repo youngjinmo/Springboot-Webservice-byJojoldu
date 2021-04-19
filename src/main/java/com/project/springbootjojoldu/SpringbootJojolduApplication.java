769bd966ecbde7293479d8973d5e76140a803e62
Youngjin Mo <dev.youngjinmo@gmail.com>
2021-04-19 23:10:10 +0900

    init gradle project

---
+++ src/main/java/com/project/springbootjojoldu/SpringbootJojolduApplication.java
@@ -0,0 +1,13 @@
+package com.project.springbootjojoldu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJojolduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJojolduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/springbootjojoldu/SpringbootJojolduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springbootjojoldu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJojolduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
