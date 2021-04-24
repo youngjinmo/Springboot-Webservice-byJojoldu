296a7b587f0045dff4568f528632fa9ccd8f09af
Youngjin Mo <dev.youngjinmo@gmail.com>
2021-04-24 17:13:06 +0900

    User Entity 생성

---
+++ src/main/java/com/project/springbootjojoldu/domain/user/User.java
@@ -0,0 +1,53 @@
+package com.project.springbootjojoldu.domain.user;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    /*
+      JPA로 DB 저장시 Enum 값을 어떤 형태로 저장할지 결정
+      기본적으로 int형으로 저장
+      숫자로 저장되면 해당 코드의 의미 파악 불가, 그래서 Enum으로 선언
+     */
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture){
+        this.name = name;
+        this.picture = picture;
+        return this;
+    }
+
+    public String getRoleKey(){
+        return this.role.getKey();
+    }
+}
