13137603e2df80b355c3371db10280e433708ce7
Youngjin Mo <dev.youngjinmo@gmail.com>
2021-04-25 12:29:29 +0900

    feat. github login 구현

--- src/main/java/com/project/springbootjojoldu/web/IndexController.java
+++ src/main/java/com/project/springbootjojoldu/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.project.springbootjojoldu.web;
 
+import com.project.springbootjojoldu.config.auth.dto.SessionUser;
+import com.project.springbootjojoldu.domain.user.User;
 import com.project.springbootjojoldu.service.posts.PostsService;
 import com.project.springbootjojoldu.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
