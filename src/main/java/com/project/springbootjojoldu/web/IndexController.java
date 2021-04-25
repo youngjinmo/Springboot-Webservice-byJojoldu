944480f0c086fb0979c6511653650775a1dedc88
Youngjin Mo <dev.youngjinmo@gmail.com>
2021-04-25 17:57:30 +0900

    feat. 커스텀 어노테이션(@LoginUser) 적용

--- src/main/java/com/project/springbootjojoldu/web/IndexController.java
+++ src/main/java/com/project/springbootjojoldu/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.project.springbootjojoldu.web;
 
+import com.project.springbootjojoldu.config.auth.LoginUser;
 import com.project.springbootjojoldu.config.auth.dto.SessionUser;
 import com.project.springbootjojoldu.domain.user.User;
 import com.project.springbootjojoldu.service.posts.PostsService;
@@ -20,9 +21,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
