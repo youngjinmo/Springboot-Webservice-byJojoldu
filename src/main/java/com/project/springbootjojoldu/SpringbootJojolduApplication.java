185c9f0d0b6d7126626b24383dfb5ff6e19a8013
Youngjin Mo <dev.youngjinmo@gmail.com>
2021-04-26 12:41:58 +0900

    refactor. 테스트 환경위한 JpaConfig 분리

--- src/main/java/com/project/springbootjojoldu/SpringbootJojolduApplication.java
+++ src/main/java/com/project/springbootjojoldu/SpringbootJojolduApplication.java
@@ -2,9 +2,7 @@ package com.project.springbootjojoldu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class SpringbootJojolduApplication {
     public static void main(String[] args) {
