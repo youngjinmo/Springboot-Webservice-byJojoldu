a2f9e1af8eba48ccf502bb7ebf9ebc918c4d437a
Youngjin Mo <dev.youngjinmo@gmail.com>
2021-04-26 13:35:24 +0900

    fix. Github, Naver OAuth 로그인시 프로필 이미지 못가져오는 현상 해결

--- src/main/java/com/project/springbootjojoldu/web/IndexController.java
+++ src/main/java/com/project/springbootjojoldu/web/IndexController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
